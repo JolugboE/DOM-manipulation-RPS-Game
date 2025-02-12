6bb9c75bbc74a662cb1910e41deb80ed64a78777
JolugboE <jolugboesther@gmail.com>
2025-02-12 08:52:26 +0100

    A new game project

--- script.js
+++ script.js
@@ -35,3 +35,9 @@ const roundResultsMsg = document.getElementById("results-msg");
 const winnerMsgElement = document.getElementById("winner-msg");
 const optionsContainer = document.querySelector(".options-container");
 const resetGameBtn = document.getElementById("reset-game-btn");
+
+function showResults(userOption) {
+    roundResultsMsg.innerText = getRoundResults(userOption);
+    computerScoreSpanElement.innerText = computerScore;
+    playerScoreSpanElement.innerText = playerScore;
+  
