f44581dc2b94515bd7320c5b9d84de5f2988fb6d
JolugboE <jolugboesther@gmail.com>
2025-02-12 08:56:53 +0100

    A new game project completed

--- script.js
+++ script.js
@@ -51,3 +51,32 @@ function showResults(userOption) {
   }
 
 };
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreSpanElement.innerText = playerScore;
+    computerScoreSpanElement.innerText = computerScore;
+    roundResultsMsg.innerText = "";
+    winnerMsgElement.innerText = "";
+  
+    resetGameBtn.style.display = "none"; 
+    optionsContainer.style.display = "block"; 
+  }
+  
+  resetGameBtn.addEventListener("click", resetGame);
+  
+  const rockBtn = document.getElementById("rock-btn");
+  const paperBtn = document.getElementById("paper-btn");
+  const scissorsBtn = document.getElementById("scissors-btn");
+  
+  rockBtn.addEventListener("click", function () {
+    showResults("Rock");
+  });
+  
+  paperBtn.addEventListener("click", function () {
+    showResults("Paper");
+  });
+  
+  scissorsBtn.addEventListener("click", function () {
+    showResults("Scissors");
+  });
